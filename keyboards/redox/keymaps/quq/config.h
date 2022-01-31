24db98e4b20585043f10c2819f553190f30fba4a
quq <quq@magisystem.xyz>
2022-01-31 11:03:18 +0100

    Added keymap and changed makefile

---
+++ keyboards/redox/keymaps/quq/config.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Change USB Descriptor for fun */
+#undef VENDOR_ID
+#define VENDOR_ID       0x5151 // "QQ"
+#undef PRODUCT_ID
+#define PRODUCT_ID      0x5244 // "RD"
+#undef DEVICE_VER
+#define DEVICE_VER      0x0100
+#undef MANUFACTURER
+#define MANUFACTURER    quq
+#undef PRODUCT
+#define PRODUCT         Redox Keyboard
+
+/* Use I2C for comunication */
+#define USE_I2C
+
+/* Read handedness from EEPROM */
+#define EE_HANDS
+
+/* Remove Cherry MX Lock Support */
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+/* Use 8 bit to track layer */
+#define LAYER_STATE_8BIT
--- keyboards/redox/rules.mk
+++ keyboards/redox/rules.mk
@@ -1,5 +1,30 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MAGIC_ENABLE = yes          # Enable Magic
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Disable WS2812 RGB underlight.
+
+SPLIT_KEYBOARD = yes
+
 DEFAULT_FOLDER = redox/rev1/base
 
 # Disable unsupported hardware
 AUDIO_SUPPORTED = no
 BACKLIGHT_SUPPORTED = no
+
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Enable LTO
+LTO_ENABLE = yes
